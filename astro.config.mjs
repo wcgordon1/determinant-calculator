d9d6d03a1beed433300b9cc2d2aead237dc1a05d
wcgordon1 <wcgordon1@gmail.com>
2025-02-03 12:19:11 -0800

    fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+import react from '@astrojs/react';
 
 export default defineConfig({
   site: 'https://determinantcalculator.com', // Replace with your actual domain
   trailingSlash: 'never', // Enforce no trailing slash
   integrations: [
     tailwind(),
-    sitemap()
+    sitemap(),
+    react()
   ]
 });
