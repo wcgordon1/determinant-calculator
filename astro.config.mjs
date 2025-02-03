ea8aa8229124e2bd3db748a67bbf13762c884d08
wcgordon1 <wcgordon1@gmail.com>
2025-02-03 12:13:46 -0800

    fixe dbuild maybe

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
+import cloudflare from '@astrojs/cloudflare';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  site: 'https://determinantcalculator.com', // Replace with your actual domain
+  output: 'server',
+  adapter: cloudflare(),
+  integrations: [tailwind()]
 });
